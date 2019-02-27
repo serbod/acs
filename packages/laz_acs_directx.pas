2db030f8d1bc2d2077f7d8855af7ae2f1c66c13f
Sergey Bodrov <serbod@gmail.com>
2019-02-28 00:47:44 +0300

    Added laz_acs_directx optional package

---
+++ packages/laz_acs_directx.pas
@@ -0,0 +1,15 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit laz_acs_directx;
+
+{$warn 5023 off : no warning about unused units}
+interface
+
+uses
+  acs_dxaudio, acs_dsfiles;
+
+implementation
+
+end.
